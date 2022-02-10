21422b2311d778df5f8faa4fd0a8eee321c3e7d3
panyue <90938238g@gmail.com>
2022-02-10 16:30:43 +0800

    依赖文件(vue.js/vue.min.js)

--- 19_单文件组件/App.vue
+++ 19_单文件组件/App.vue
@@ -1,21 +1,20 @@
 <template>
-   <div>
-     <School></School>
-     <Student></Student>
-   </div>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
 </template>
 
 <script>
-import School from './School';
+import School from "./School";
 import Student from "./Student";
 
 export default {
   name: "App",
   //汇总所有的组件
-  components:{
+  components: {
     Student,
-    School
-  }
-}
+    School,
+  },
+};
 </script>
-
